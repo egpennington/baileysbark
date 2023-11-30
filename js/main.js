4cc8599f7247bf7630482f07c237e72d9dd4cbd5
egpennington (애멧) <53251137+egpennington@users.noreply.github.com>
2023-11-30 16:47:21 -0600

    Update main.js
    added js for random background from image.js file

--- js/main.js
+++ js/main.js
@@ -1,14 +1,16 @@
 import quotes from "./quotes.js"
+import imagesFile from './images.js'
+
 // set up variables
 const authorEl = document.getElementById("author")
 const quoteEl = document.getElementById("quote")
 const btnEl = document.getElementById("btn")
 
-
 // function to generate quotes
 function generateRandomQuote() {
     const randomIndex = Math.floor(Math.random() * quotes.length)
     const randomQuote = quotes[randomIndex]
+    const randomImage = generateImage()
 
     quoteEl.innerHTML = `
             <h1>${randomQuote.quote}</h1>
@@ -17,14 +19,19 @@ function generateRandomQuote() {
     authorEl.innerHTML = `
             <h2 class="rt-side">- ${randomQuote.author}</h2>
             `
-    document.body.style.background = "url('https://source.unsplash.com/random/1920x1280/?water,nature')"
+    document.body.style.backgroundImage = `url('${randomImage}')` 
 }
 
-// random quote at start
-generateRandomQuote()
+// function to generate random images from js file
+function generateImage() {
+        const randomIndex = Math.floor(Math.random() * imagesFile.length);
+        const imagePath = `./${imagesFile[randomIndex]}`;
+        return imagePath;
+    }
 
 // event listener for button
 btnEl.addEventListener("click", function() {
         location.reload()
 })
 
+generateRandomQuote()
